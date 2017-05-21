e5dc4fa55e873a95cdf654b34ac2edf863eb0246
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-21 01:23:27 -0600

    Initial commit, stable build of ReactWeather without styling

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,19 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a82f980a1e7b69715f87ff7bd829d6a8&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+
+  return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${encodedLocation}&appid=a82f980a1e7b69715f87ff7bd829d6a8&units=imperial`).then((res) => {
+      if(res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, (res) => {
+      throw new Error(res.data.message);
+    });
+  }
+}
