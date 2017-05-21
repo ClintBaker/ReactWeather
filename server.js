0d032c521e531cc44b6f44cd2f4049b1c625504b
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-21 01:51:17 -0600

    update middleware

--- server.js
+++ server.js
@@ -5,21 +5,14 @@ const express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function (req, res, next) {
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
 
-// app.use((req, res, next) => {
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+  }
+});
 
 app.use(express.static('public'));
 
